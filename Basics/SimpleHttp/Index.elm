aa86db5be4ffae937da141e2a76511d9b5b4b35d
Svetlana Linuxenko <svetlana@linuxenko.pro>
2017-12-26 02:49:31 +0200

    Simple http request

---
+++ Basics/SimpleHttp/Index.elm
@@ -0,0 +1,92 @@
+module Index exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- model
+
+
+type alias Model =
+    { topic : String, pic : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "cats" "waiting.gif", Cmd.none )
+
+
+
+-- update
+
+
+type Msg
+    = MorePlease
+    | GetNewGif (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MorePlease ->
+            ( model, getRandomGif model.topic )
+
+        GetNewGif (Err _) ->
+            ( model, Cmd.none )
+
+        GetNewGif (Ok newUrl) ->
+            ( Model model.topic newUrl, Cmd.none )
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text model.topic ]
+        , button [ onClick MorePlease ] [ text "More Please!" ]
+        , br [] []
+        , img [ src model.pic ] []
+        ]
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url =
+            "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+    in
+        Http.send GetNewGif (Http.get url decodeGifUrl)
+
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+    Decode.at [ "data", "image_url" ] Decode.string
